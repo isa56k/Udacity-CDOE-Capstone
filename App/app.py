244fc8c923c92b185dcb7797a402e52cd55d5fc1
Rob Kersey <rob@phazeonemobile.com>
2020-06-11 19:15:50 +0100

    Jenkins File Changes

--- App/app.py
+++ App/app.py
@@ -1,10 +1,10 @@
-import platform
+import platform, sys
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html",version=sys.argv[1])
 
 @app.route('/hostname')
 def hostname():
